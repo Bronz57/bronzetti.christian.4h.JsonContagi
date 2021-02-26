3d2f8d0f569df45fd7ae6723f8eac64f69b60b72
Bronz57 <chbronz88@gmail.com>
2021-02-26 14:27:32 +0100

    Primo commit

---
+++ bronzetti.christian.4h.JSONContagi/Models/Statistica.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace bronzetti.christian._4h.JSONContagi
+{
+    class Statistica
+    {
+        public string Data { get; set; }
+        public int Progressivo { get; set; }
+        public int Contagi { get; set; }
+
+        //costruttore con parametri
+        public Statistica(string d, int p, int c)
+        {
+            Data = d;
+            Progressivo = p;
+            Contagi = c;
+        }
+
+        public Statistica() { }
+       public void TrasformaDataInFormatoItaliano()
+       {
+
+            //prendo la data in string e la converto in dateTime
+            DateTime retVal = DateTime.ParseExact(Data.ToString(), "M/d/yy", CultureInfo.GetCultureInfoByIetfLanguageTag("en-GB"));
+
+            //per ogni componente della data salvo i dati
+            int giorno = retVal.Day;
+            int mese = retVal.Month;
+            int anno = retVal.Year;
+
+            //riscrivo
+            Data = $"{giorno}/{mese}/{anno}";
+       }
+    }
+}
---
+++ bronzetti.christian.4h.JSONContagi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+namespace bronzetti.christian._4h.JSONContagi
+{
+    //1. Trasformare il file JSON fornito in un secondo file in cui il formato Data risulta conforme allo
+    //standard italiano.
+    class Program
+    {
+        static string LeggiFileJson(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+                return sr.ReadToEnd();
+        }
+
+        static void Main(string[] args)
+        {
+            //percoso del file
+            string path = @"ContagiRimini.json";
+
+            //copio tutto il file json in una stringa
+            string statisticheJson = LeggiFileJson(path);
+
+            //deserializzo
+            var objectDeserialized = JsonConvert.DeserializeObject<List<Statistica>>(statisticheJson);
+
+            //per ogni data la converto nello stile italiano
+            foreach (var s in objectDeserialized)
+                s.TrasformaDataInFormatoItaliano();
+
+            //riserializzo tutto
+             string prova = JsonConvert.SerializeObject(objectDeserialized, Formatting.Indented);
+            
+            //scrivo nel file nuovo 
+            File.WriteAllText("ContagiRiminiRiformattato.json", prova.ToString());
+
+            Console.WriteLine("Dati salvati... Controllare nel seguente percoso bin/debug/'ContagiRiminiRiformattato.json'");
+        }
+    }
+}
